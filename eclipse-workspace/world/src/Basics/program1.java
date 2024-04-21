661292e8486895feb76b0c6b02d59e4b8811a14e
pavan <pavanthogata85@gmail.com>
2024-04-21 16:51:01 +0530

    message

---
+++ eclipse-workspace/world/src/Basics/program1.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class program1 {
+	
+	public static void main(String[]args) {
+		System.out.println(123);
+		System.out.println(3.14);
+		System.out.println('A');
+		System.out.println(false);
+		
+	}
+
+}
