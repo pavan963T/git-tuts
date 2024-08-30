278053245fbe3a3abf11244b2989acdd9f91cc60
pavan <pavanthogata85@gmail.com>
2024-08-30 17:20:06 +0530

    dcbuhb

---
+++ src/main/java/com/GratsTech2/SpringBootProject03Application.java
@@ -0,0 +1,13 @@
+package com.GratsTech2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject03Application.class, args);
+	}
+
+}
