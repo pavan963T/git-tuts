523183d5e31ee5230d3e34225cca3fd5476ab5e6
pavan <pavanthogata85@gmail.com>
2024-08-30 17:27:58 +0530

    added to git !

---
+++ src/main/java/com/GratsTech2/Entity/Users.java
@@ -0,0 +1,57 @@
+package com.GratsTech2.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name ="usersTable")
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	
+	public Users() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Users(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
