76f1d780bcbd9bea0d8012ffe2c1e0cfb17bd00e
pavan <pavanthogata85@gmail.com>
2024-09-01 13:08:33 +0530

    deleted src

--- src/main/java/com/GratsTech2/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.GratsTech2.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.GratsTech2.Entity.Users;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
